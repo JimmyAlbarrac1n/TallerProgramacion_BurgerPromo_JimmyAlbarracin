fbeac827279bfb38f0ca24e7b437b29a642ee38c
laboratorio <laboratorio@UPOAULA10303>
2024-05-08 11:42:19 -0500

    Agregar archivos de proyecto.

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TallerProgramacion.Models;
+
+namespace Taller
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TallerProgramacion.Models.Burger> Burger { get; set; } = default!;
+        public DbSet<TallerProgramacion.Models.Promo> Promo { get; set; } = default!;
+    }
+}
---
+++ Models/Promo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerProgramacion.Models
+{
+    public class Promo
+    {
+        [Key]
+        public int PromoID { get; set; }
+        [Required]
+        public string? Descripcion { get; set; }
+        [Required]
+        public DateTime FechaPromo { get; set; }
+
+        public int BurgerID { get; set; }
+        public Burger? Burger { get; set; }
+    }
+}
